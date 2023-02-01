38233acffdae052d31c023327d29db9a98312d7f
WarrenT <warrentyler1024@gmail.com>
2023-02-01 16:38:17 +0000

    create today panel dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,8 @@ const searchHistoryEl = document.querySelector("#search-history");
 populateSearchHistory();
 
 function populateSearchHistory() {
-  const storedWeather = JSON.parse(localStorage.getItem("weatherForCities")) || [];
+  const storedWeather =
+    JSON.parse(localStorage.getItem("weatherForCities")) || [];
   storedWeather.forEach((weather) => {
     addToSearchHistory(weather.city.name);
     // console.log(city.name);
@@ -35,6 +36,53 @@ function addToSearchHistory(cityName) {
   }
 }
 
+// Function to save cities to localStorage
+function addWeatherToLocalStorage(cityWeather) {
+  const storedWeather =
+    JSON.parse(localStorage.getItem("weatherForCities")) || [];
+  if (
+    !storedWeather.some(
+      (weather) => weather.city.name === cityWeather.city.name
+    )
+  ) {
+    storedWeather.push(cityWeather);
+  }
+  localStorage.setItem("weatherForCities", JSON.stringify(storedWeather));
+}
+
+function populateTodayPanel(weather) {
+  const todayPanelEl = document.querySelector("#today-panel");
+
+  todayPanelEl.innerHTML = createTodayPanelHTML(weather);
+}
+
+function getWeatherIconURL(weather) {
+  return `http://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png`;
+}
+
+function convertKelvinToCelcius(kelvin) {
+  return kelvin - 273.15;
+}
+
+function createTodayPanelHTML(weather) {
+  return `
+    <h2 id="city-name" class="display-4">${weather.city.name}</h2>
+    <div class="card">
+      <h2 class="card-header h3">
+      ${moment.unix(weather.list[0].dt).format(
+        "DD/MM/YYYY"
+      )}
+      </h2>
+      <div class="card-body">
+        <img src="${getWeatherIconURL(weather)}" />
+        <p>Temperature: ${convertKelvinToCelcius(weather.list[0].main.temp).toFixed(2)}<sup>o</sup>C</p>
+        <p>Wind: ${weather.list[0].wind.speed}</p>
+        <p>Humidity: ${weather.list[0].main.humidity}</p>
+      </div>
+    </div>
+  `;
+}
+
 // addEventListener on search button
 searchButtonEl.addEventListener("click", function (event) {
   event.preventDefault();
@@ -52,7 +100,7 @@ searchButtonEl.addEventListener("click", function (event) {
     .then((response) => response.json())
     .then((citiesFound) => {
       console.log("City found: " + !citiesFound.length);
-      
+
       // if (!citiesFound.length) return;
       const firstCity = citiesFound[0];
       console.log("firstCity: " + firstCity.name);
@@ -70,17 +118,8 @@ searchButtonEl.addEventListener("click", function (event) {
       console.log(cityWeather);
 
       addWeatherToLocalStorage(cityWeather);
+      populateTodayPanel(cityWeather);
       addToSearchHistory(cityWeather.city.name);
     })
-    .catch(err => console.log(err));
-
-  // Function to save cities to localStorage
-  function addWeatherToLocalStorage(cityWeather) {
-    
-    const storedWeather = JSON.parse(localStorage.getItem("weatherForCities")) || [];
-    if (!storedWeather.some((weather) => weather.city.name === cityWeather.city.name)) {
-      storedWeather.push(cityWeather);
-    }
-    localStorage.setItem("weatherForCities", JSON.stringify(storedWeather));
-  }
+    .catch((err) => console.log(err));
 });
